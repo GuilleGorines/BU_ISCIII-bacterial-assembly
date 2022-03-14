386c1c436caeae9f19ffbfa4d08f20f80e1d7b0e
GuilleGorines <guillermo.gorines@gmail.com>
2022-03-14 18:41:19 +0100

    linting

--- bin/parse_kmerfinder.py
+++ bin/parse_kmerfinder.py
@@ -9,16 +9,15 @@ import pickle
 import os
 
 
-
 #################
 ### FUNCTIONS ###
 #################
 
 
-def check_arg (args=None) :
+def check_arg(args=None):
 
-    '''
-	Description:
+    """
+        Description:
         Function collect arguments from command line using argparse
     Input:
         args # command line arguments
@@ -28,20 +27,32 @@ def check_arg (args=None) :
         parser
     Return
         parser.parse_args() # Parsed arguments
-    '''
+    """
 
-    parser = argparse.ArgumentParser(prog = '07-kmerfinder.py', formatter_class=argparse.RawDescriptionHelpFormatter, description= '07-kmerfinder.py creates a csv file from results.txt file')
+    parser = argparse.ArgumentParser(
+        prog="07-kmerfinder.py",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="07-kmerfinder.py creates a csv file from results.txt file",
+    )
 
-    parser.add_argument('--path','-p', required=True, help='Insert path of results.txt file like /home/user/Service_folder/ANALYSIS/07-kmerfinder')
+    parser.add_argument(
+        "--path",
+        "-p",
+        required=True,
+        help="Insert path of results.txt file like /home/user/Service_folder/ANALYSIS/07-kmerfinder",
+    )
 
-    parser.add_argument('--output_bn','-b', required=True, help='The output in binary file')
+    parser.add_argument(
+        "--output_bn", "-b", required=True, help="The output in binary file"
+    )
 
-    parser.add_argument('--output_csv','-c', required=True, help='The output in csv file')
+    parser.add_argument(
+        "--output_csv", "-c", required=True, help="The output in csv file"
+    )
 
-   #Example: python3 parse_kmerfinder.py -p /home/s.gonzalez/07-kmerfinder -b p_dic.dicke -c p_kmer.csv
+    # Example: python3 parse_kmerfinder.py -p /home/s.gonzalez/07-kmerfinder -b p_dic.dicke -c p_kmer.csv
 
     return parser.parse_args()
-
 
 
 #################
@@ -49,48 +60,46 @@ def check_arg (args=None) :
 #################
 
 
-def kmerfinder_dictionary (file_txt):
+def kmerfinder_dictionary(file_txt):
 
-    '''
+    """
     Description:
         Function to extract the relevant part of result.txt file
     Input:
         result.txt file
     Return:
         dictionary
-    '''
+    """
 
-    step = '07-kmerfinder_'
+    step = "07-kmerfinder_"
 
-    num_lines = sum(1 for line in open (file_txt))
-    hits = (num_lines - 1) #to count the total number of hits
-    lookupfile = open (file_txt, 'r')
+    num_lines = sum(1 for line in open(file_txt))
+    hits = num_lines - 1  # to count the total number of hits
+    lookupfile = open(file_txt, "r")
     lines = lookupfile.readlines()
     parameters = lines[0].strip().split("\t")
     if num_lines > 1:
         values_best_hit = lines[1].strip().split("\t")
-    if num_lines >2:
+    if num_lines > 2:
         values_second_hit = lines[2].strip().split("\t")
 
+    kmer_dict = {}
 
-    kmer_dict ={}
-
-
-    for i in range (len(parameters)):
+    for i in range(len(parameters)):
         if num_lines > 1:
-            kmer_dict[step + 'best_hit_' + parameters [i]] = values_best_hit [i]
+            kmer_dict[step + "best_hit_" + parameters[i]] = values_best_hit[i]
         else:
-            kmer_dict[step + 'best_hit_' + parameters [i]] = ''
+            kmer_dict[step + "best_hit_" + parameters[i]] = ""
 
-        kmer_dict.update(Total_hits_07_kmerfinder = hits)
+        kmer_dict.update(Total_hits_07_kmerfinder=hits)
 
-        if num_lines >2:
+        if num_lines > 2:
 
-            kmer_dict [step + 'second_hit_' + parameters [i]] = values_second_hit [i]
+            kmer_dict[step + "second_hit_" + parameters[i]] = values_second_hit[i]
 
         else:
 
-            kmer_dict [step + 'second_hit_' + parameters [i]] = ''
+            kmer_dict[step + "second_hit_" + parameters[i]] = ""
 
     return kmer_dict
 
@@ -100,9 +109,9 @@ def kmerfinder_dictionary (file_txt):
 #################
 
 
-def dictionary2bn (dictionary, binary_file):
+def dictionary2bn(dictionary, binary_file):
 
-    '''
+    """
 
     Description:
         Function to create a binary file from a dictionary
@@ -110,23 +119,23 @@ def dictionary2bn (dictionary, binary_file):
         dictionary
     Return:
         binary file
-    '''
+    """
 
-
-    pickle_out = open(binary_file,"wb")
+    pickle_out = open(binary_file, "wb")
     pickle.dump(dictionary, pickle_out)
     pickle_out.close()
 
     return
+
 
 #################
 ### FUNCTIONS ###
 #################
 
 
-def dictionary2csv (dictionary, csv_file):
+def dictionary2csv(dictionary, csv_file):
 
-    '''
+    """
 
     Description:
         Function to create a csv from a dictionary
@@ -135,15 +144,14 @@ def dictionary2csv (dictionary, csv_file):
     Return:
         csv file
 
-   '''
-
+    """
 
     header = sorted(set(i for b in map(dict.keys, dictionary.values()) for i in b))
-    with open(csv_file, 'w', newline="") as f:
+    with open(csv_file, "w", newline="") as f:
         write = csv.writer(f)
-        write.writerow(['sample_name', *header])
+        write.writerow(["sample_name", *header])
         for a, b in dictionary.items():
-            write.writerow([a]+[b.get(i, '') for i in header])
+            write.writerow([a] + [b.get(i, "") for i in header])
     return
 
 
@@ -152,46 +160,43 @@ def dictionary2csv (dictionary, csv_file):
 ###################
 
 
-if __name__ == '__main__' :
-
+if __name__ == "__main__":
 
     # Variables
-    version = '07-kmerfinder.py v 0.1.0.'  # Script version
+    version = "07-kmerfinder.py v 0.1.0."  # Script version
     arguments = check_arg(sys.argv[1:])
 
     # Create sample_id_list
     path = arguments.path
     sample_list = []
-    tmp = os.listdir (path)
+    tmp = os.listdir(path)
     for item in tmp:
         if os.path.isdir(os.path.join(path, item)):
-            if item != 'logs':
+            if item != "logs":
                 sample_list.append(item)
 
-    print ('sample_list done')
-    #print (sample_list)
+    print("sample_list done")
+    # print (sample_list)
 
     # Create a dictionary
     kmer_all = {}
 
-
     for sample in sample_list:
 
-        file_name = os.path.join (path,sample,"results.txt")
+        file_name = os.path.join(path, sample, "results.txt")
         kmer_all[sample] = kmerfinder_dictionary(file_name)
 
-    print ('kmerfinder_dictionary done')
-    #print (kmer_all)
-
+    print("kmerfinder_dictionary done")
+    # print (kmer_all)
 
     # Save the dicctionary to binary file
 
-    dictionary2bn (kmer_all, arguments.output_bn)
+    dictionary2bn(kmer_all, arguments.output_bn)
 
-    print ('kmerfinder_dictionary_bn done')
+    print("kmerfinder_dictionary_bn done")
 
     # Convert the dictionary to csv file
 
-    dictionary2csv (kmer_all, arguments.output_csv)
+    dictionary2csv(kmer_all, arguments.output_csv)
 
-    print ('kmerfinder_dictionary_csv done')
+    print("kmerfinder_dictionary_csv done")
