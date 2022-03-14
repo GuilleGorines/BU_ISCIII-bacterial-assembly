54fa030f6b9e423594ee2bf57f931e4d2c771da7
GuilleGorines <guillermo.gorines@gmail.com>
2022-03-14 19:00:13 +0100

    linting according to pep8

--- bin/check_samplesheet.py
+++ bin/check_samplesheet.py
@@ -7,8 +7,10 @@ import argparse
 
 
 def parse_args(args=None):
-    Description = "Reformat nf-core/viralrecon samplesheet file and check its contents."
-    Epilog = """Example usage: python check_samplesheet.py <FILE_IN> <FILE_OUT>"""
+    Description = \
+        "Reformat nf-core/viralrecon samplesheet file and check its contents."
+    Epilog = \
+        """Example usage: python check_samplesheet.py <FILE_IN> <FILE_OUT>"""
 
     parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
     parser.add_argument("FILE_IN", help="Input samplesheet file.")
@@ -27,13 +29,14 @@ def make_dir(path):
 
 def print_error(error, line):
     print(
-        "ERROR: Please check samplesheet -> {}\nLine: '{}'".format(error, line.strip())
+        "ERROR: Please check samplesheet -> \
+            {}\nLine: '{}'".format(error, line.strip())
     )
     sys.exit(1)
 
 
 def check_samplesheet(FileIn, FileOut):
-    ## Check header
+    # Check header
     HEADER = ["sample", "fastq_1", "fastq_2"]
     fin = open(FileIn, "r")
     header = fin.readline().strip().split(",")
@@ -51,18 +54,20 @@ def check_samplesheet(FileIn, FileOut):
         if line:
             lspl = [x.strip() for x in line.strip().split(",")]
 
-            ## Check valid number of columns per row
+            # Check valid number of columns per row
             if len(lspl) != len(header):
                 print_error(
-                    "Invalid number of columns (minimum = {})!".format(len(header)),
+                    "Invalid number of columns \
+                    (minimum = {})!".format(len(header)),
                     line,
                 )
 
             numCols = len([x for x in lspl if x])
             if numCols < 2:
-                print_error("Invalid number of populated columns (minimum = 2)!", line)
+                print_error("Invalid number of populated columns \
+                    (minimum = 2)!", line)
 
-            ## Check sample name entries
+            # Check sample name entries
             sample, fastQFiles = lspl[0], lspl[1:]
             if sample:
                 if sample.find(" ") != -1:
@@ -70,25 +75,26 @@ def check_samplesheet(FileIn, FileOut):
             else:
                 print_error("Sample entry has not been specified!", line)
 
-            ## Check FastQ file extension
+            # Check FastQ file extension
             for fastq in fastQFiles:
                 if fastq:
                     if fastq.find(" ") != -1:
                         print_error("FastQ file contains spaces!", line)
                     if fastq[-9:] != ".fastq.gz" and fastq[-6:] != ".fq.gz":
                         print_error(
-                            "FastQ file does not have extension '.fastq.gz' or '.fq.gz'!",
+                            "FastQ file does not have extension \
+                            '.fastq.gz' or '.fq.gz'!",
                             line,
                         )
 
-            ## Auto-detect paired-end/single-end
+            # Auto-detect paired-end/single-end
             sample_info = (
                 []
-            )  ## [single_end, is_sra, is_ftp, fastq_1, fastq_2, md5_1, md5_2]
+            )  # [single_end, is_sra, is_ftp, fastq_1, fastq_2, md5_1, md5_2]
             fastq_1, fastq_2 = fastQFiles
-            if sample and fastq_1 and fastq_2:  ## Paired-end short reads
+            if sample and fastq_1 and fastq_2:  # Paired-end short reads
                 sample_info = ["0", "0", "0", fastq_1, fastq_2, "", ""]
-            elif sample and fastq_1 and not fastq_2:  ## Single-end short reads
+            elif sample and fastq_1 and not fastq_2:  # Single-end short reads
                 sample_info = ["1", "0", "0", fastq_1, fastq_2, "", ""]
             else:
                 print_error("Invalid combination of columns provided!", line)
@@ -104,7 +110,7 @@ def check_samplesheet(FileIn, FileOut):
             fin.close()
             break
 
-    ## Write validated samplesheet with appropriate columns
+    # Write validated samplesheet with appropriate columns
     if len(sampleRunDict) > 0:
         OutDir = os.path.dirname(FileOut)
         make_dir(OutDir)
@@ -126,9 +132,11 @@ def check_samplesheet(FileIn, FileOut):
         )
         for sample in sorted(sampleRunDict.keys()):
 
-            ## Check that multiple runs of the same sample are of the same datatype
+            # Check that multiple runs of the same sample
+            # are of the same datatype
             if not all(
-                x[:2] == sampleRunDict[sample][0][:2] for x in sampleRunDict[sample]
+                x[:2] == sampleRunDict[sample][0][:2]
+                for x in sampleRunDict[sample]
             ):
                 print_error(
                     "Multiple runs of a sample must be of the same datatype",
@@ -136,7 +144,9 @@ def check_samplesheet(FileIn, FileOut):
                 )
 
             for idx, val in enumerate(sampleRunDict[sample]):
-                fout.write(",".join(["{}_T{}".format(sample, idx + 1)] + val) + "\n")
+                text = ",".join(["{}_T{}".format(sample, idx + 1)] + val)
+                + "\n"
+                fout.write(text)
         fout.close()
 
 
